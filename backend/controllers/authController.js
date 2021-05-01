055265bd13325f0f020b15066078dd163c6c52a4
Abdullah <abdnadeem382@gmail.com>
2021-05-01 09:44:22 +0500

    Added admin functions to update, delete users. User function to update profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -116,3 +116,105 @@ exports.resetPassword = catchAsyncErrors(async (req,res,next)=>{
     await user.save()
     sendToken(user,200,res); 
 })
+
+exports.getUSerProfile = catchAsyncErrors(async(req,res,next)=>{
+    const user  = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+
+exports.updatePassword = catchAsyncErrors(async(req,res,next)=>{
+    const user  = await User.findById(req.user.id).select('+password');
+    const isMatched = await user.comparePassword(req.body.oldPassword);
+    if(!isMatched){
+        return next(new ErrorHandler('Old password is incorrect',400))
+    }
+
+    user.password = req.body.password;
+    await user.save();
+    sendToken(user,200,res);
+})
+
+exports.updateProfile = catchAsyncErrors(async(req,res,next)=>{
+   const newUserData = {
+       name: req.body.name,
+       email: req.body.email
+   }
+
+   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+       new: true,
+       runValidators: true,
+       useFindAndModify: false
+   });
+
+   res.status(200).json({
+       success: true,
+       user
+   })
+})
+
+//ADMIN ROUTES
+
+exports.getAllUsers = catchAsyncErrors(async(req,res,next)=>{
+    const users = await User.find();
+    res.status(200).json({
+        success: true,
+        users
+    })
+})
+
+
+exports.getUser = catchAsyncErrors(async(req,res,next)=>{
+    const user = await User.findById(req.params.id);
+    
+    if(!user){
+        return next(new ErrorHandler(`User not found with id ${req.params.id}`, 400))
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+
+exports.updateUser = catchAsyncErrors(async(req,res,next)=>{
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role
+    }
+
+ 
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+ 
+    res.status(200).json({
+        success: true,
+        user
+    })
+ })
+
+ exports.deleteUser = catchAsyncErrors(async(req,res,next)=>{
+    
+    const user = await User.findById(req.params.id);
+    
+    if(!user){
+        return next(new ErrorHandler(`User not found with id ${req.params.id}`, 400))
+    }
+
+    // TODO: Remove avater from cloudinary
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+ })
